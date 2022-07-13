c686beae25815c9f6e4ed476a122d0ca5c2d5cda
The1Kwa1Jsucsh <the1kwa1jsucsh@gmail.com>
2022-07-12 20:09:19 -0400

    Finish of phone design

--- src/Components/Demos/ToastyPhone/ToastyPhone.jsx
+++ src/Components/Demos/ToastyPhone/ToastyPhone.jsx
@@ -5,16 +5,14 @@ import {MathUtils} from "three";
 import structuredClone from '@ungap/structured-clone';
 import { parseInput } from "./InputParser";
 import { FaSignal } from 'react-icons/fa';
-import { BiWifi } from 'react-icons/bi';
-import { BsBatteryFull } from "react-icons/bs"
+import {IoIosArrowForward, IoIosArrowBack, IoIosBatteryFull, IoIosWifi} from "react-icons/io"
 
 const ToastyPhone = () => {
   const [input, updateInput] = useState("");
   const [messageSequence, updateMessages] = useState([]);
   const [lastSender, setLastSender] = useState(null);
-  const [curTime, setCurTime] = useState(new Date().toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: false }))
-
-  let timeAtLoad = new Date().toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
+  const [curTime, setCurTime] = useState(new Date().toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: false }));
+  const [timeAtLoad] = useState(new Date().toLocaleTimeString([], { hour: 'numeric', minute: '2-digit'}));
 
   useEffect(() => {
     const elem = document.getElementById('chat');
@@ -22,6 +20,13 @@ const ToastyPhone = () => {
       elem.scrollTop = elem.scrollHeight;
     }
   }, [messageSequence]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurTime(new Date().toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: false }));
+    }, 10*1000);
+    return () => clearInterval(interval);
+  }, []);
 
   const handleSubmit = function (event) {
     event.preventDefault();
@@ -81,17 +86,27 @@ const ToastyPhone = () => {
           </div>
           <div className="grid-item" />
           <div className="grid-item">
-            <p className="center iosFont">
-              <FaSignal />
-              <BiWifi />
-              <BsBatteryFull />
+            <p className="center iosFont" style={{width: "100%", textAlign: "right"}}>
+              <FaSignal style={{marginRight: "4px"}}/>
+              <IoIosWifi style={{marginRight: "4px"}}/>
+              <IoIosBatteryFull />
             </p>
           </div>
           <div className="grid-item">
-            <p className="center iosFont">4</p>
+            <p className="center iosFont" style={{left: "5%", top: "25%"}}>
+              <IoIosArrowBack style={{fontSize: "22px"}} fill="#1777e2"/>
+            </p>
           </div>
           <div className="grid-item">
-            <p className="center iosFont">5</p>
+            <div className="center" style={{height: "100%"}}>
+              <div className="avatarHolder">
+                <img src="https://pbs.twimg.com/profile_images/1521592428364505090/7VVIjH5j_400x400.jpg" className="avatar"/>
+              </div>
+              <p className="avatarName">
+                Toasty
+                <IoIosArrowForward fill="#818181" style={{transform: "translate(0, 2px)"}}/>
+              </p>
+            </div>
           </div>
           <div className="grid-item" />
         </div>
