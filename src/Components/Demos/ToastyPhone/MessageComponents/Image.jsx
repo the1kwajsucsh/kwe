92a3532693a6efbc32ad9780f25a788655c66629
The1Kwa1Jsucsh <the1kwa1jsucsh@gmail.com>
2022-07-30 21:58:09 -0400

    Scroll down on image load

--- src/Components/Demos/ToastyPhone/MessageComponents/Image.jsx
+++ src/Components/Demos/ToastyPhone/MessageComponents/Image.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 const Image = ({msg}) => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      const elem = document.getElementById('chat');
+      if (elem) {
+        elem.scrollTop = elem.scrollHeight;
+      }
+    }, 10);
+  }, []);
+
   return (
     <img className="message-image" src={msg.content} alt="Toasty Digital Art"/>
   )
