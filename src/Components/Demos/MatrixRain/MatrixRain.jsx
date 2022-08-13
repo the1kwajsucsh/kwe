1a097f83fda3afcc4268e66102ccb357309a9975
The1Kwa1Jsucsh <the1kwa1jsucsh@gmail.com>
2022-08-13 13:54:06 -0400

    Recreated effect using shader

--- src/Components/Demos/MatrixRain/MatrixRain.jsx
+++ src/Components/Demos/MatrixRain/MatrixRain.jsx
@@ -1,56 +1,95 @@
-import React, {useMemo, useRef} from "react";
+import React, {useRef} from "react";
 import {Canvas, useFrame, useLoader} from "@react-three/fiber";
 import {OrbitControls, Stats} from "@react-three/drei";
-import {BufferAttribute, DoubleSide, ImageUtils, PlaneBufferGeometry, sRGBEncoding} from "three";
+import {BufferAttribute, DoubleSide, PlaneBufferGeometry} from "three";
 import {TextureLoader} from "three/src/loaders/TextureLoader";
 import {SymbolShader} from "./SymbolShader";
 import {randInt} from "three/src/math/MathUtils";
 
-const Symbol = ({symbolFadeInterval=0.02, symbolFadePercentage=0.01, height=1, width=1, position=[0,0,0], initialOpacity=1, minNegativeOpacity=-1}) => {
-  const textureAtlas = useLoader(TextureLoader, process.env.PUBLIC_URL + "/Demos/MatrixRain/atlas.png");
-  const textureXOffset = randInt(0, 4) * 0.2;
-  const textureYOffset = randInt(0, 4) * 0.2;
+const NUM_VERT_IN_GEOM = 4;
 
-  // const ref = useRef();
-  //
-  // let prevTime = 0;
-  // let curTime;
-  //
-  // useFrame(({clock}) => {
-  //   curTime = clock.getElapsedTime();
-  //
-  //   if (curTime - prevTime > symbolFadeInterval) {
-  //     prevTime = curTime;
-  //     ref.current.opacity -= symbolFadePercentage;
-  //     if (ref.current.opacity < minNegativeOpacity) {
-  //       ref.current.opacity = 1;
-  //     }
-  //
-  //     // Random chance to change texture
-  //     if (Math.random() < 0.001) {
-  //       // ref.current.map = textures[randInt(0, textures.length-1)];
-  //     }
-  //   }
-  // });
+const getNewTextureOffset = () => {
+  return randInt(0, 4) * 0.2;
+};
 
-
-  const geometry = new PlaneBufferGeometry();
-  const count = geometry.attributes.position.count;
-  const float32Array = new Float32Array(count*2);
-  for (let i = 0; i < count*2; i +=2) {
+const initializeTextureOffsets = (textureXOffset, textureYOffset) => {
+  const float32Array = new Float32Array(NUM_VERT_IN_GEOM*2);
+  for (let i = 0; i < NUM_VERT_IN_GEOM*2; i +=2) {
     float32Array[i] = textureXOffset;
     float32Array[i+1] = textureYOffset;
   }
-  geometry.setAttribute('textureOffsets', new BufferAttribute(float32Array, 2));
+  return float32Array;
+};
 
+const updateTextureOffsets = (attributesArray) => {
+  const newXOffset = getNewTextureOffset();
+  const newYOffset = getNewTextureOffset();
+
+  for (let i = 0; i < attributesArray.size; i += 2) {
+    attributesArray[i] = newXOffset;
+    attributesArray[i+1] = newYOffset;
+  }
+};
+
+const initializeOpacity = (initialOpacity) => {
+  return new Float32Array(NUM_VERT_IN_GEOM).fill(initialOpacity)
+};
+
+const updateOpacity = (attributesArray, symbolFadeInterval, minNegativeOpacity) => {
+  let opacity = attributesArray[0] - symbolFadeInterval;
+  if (opacity < minNegativeOpacity) {
+    opacity = 1.0;
+  }
+  attributesArray.fill(opacity)
+};
+
+const Symbol = ({symbolFadeInterval=0.02, symbolFadePercentage=0.01, height=1, width=1, position=[0,0,0], initialOpacity=1, minNegativeOpacity=-1}) => {
+  const materialRef = useRef();
+  const geometryRef = useRef();
+  const textureAtlas = useLoader(TextureLoader, process.env.PUBLIC_URL + "/Demos/MatrixRain/atlas.png");
+  const textureXOffset = getNewTextureOffset();
+  const textureYOffset = getNewTextureOffset();
+
+  let prevTime = 0;
+  let curTime;
+  useFrame(({clock}) => {
+    curTime = clock.getElapsedTime();
+    if (curTime - prevTime > symbolFadeInterval) {
+      prevTime = curTime;
+
+      // update opacity for falling effect
+      updateOpacity(geometryRef.current.attributes.opacity.array, symbolFadeInterval, minNegativeOpacity);
+      geometryRef.current.attributes.opacity.needsUpdate = true;
+
+
+      // Random chance to change texture
+      if (Math.random() < 0.001) {
+        updateTextureOffsets(geometryRef.current.attributes.textureOffsets.array);
+        geometryRef.current.attributes.textureOffsets.needsUpdate = true;
+      }
+    }
+
+    materialRef.current.uniforms.u_time.value = clock.getElapsedTime();
+  });
 
   return (
-    <mesh position={position} rotation={[0, 0, 0]} scale={[1, height, width]} geometry={geometry}>
+    <mesh position={position} rotation={[0, 0, 0]} scale={[1, height, width]}>
+      <planeBufferGeometry ref={geometryRef}>
+        <bufferAttribute
+          attach="attributes-textureOffsets"
+          {...new BufferAttribute(initializeTextureOffsets(textureXOffset, textureYOffset), 2)}
+        />
+        <bufferAttribute
+          attach="attributes-opacity"
+          {...new BufferAttribute(initializeOpacity(initialOpacity), 1)}
+        />
+      </planeBufferGeometry>
       <shaderMaterial
+        ref={materialRef}
         attach="material"
         args={[SymbolShader]}
         uniforms-textureAtlas-value={textureAtlas}
-        uniforms-initialOpacity-value={1.0}
+        uniforms-u_time-value={0.0}
         side={DoubleSide}
         transparent
       />
@@ -78,7 +117,7 @@ const SymbolPlane = ({numColumns=25}) => {
   const columns = Array(numColumns).fill(false);
 
   return (
-    <group position={[numColumns/2, numColumns/2, 0]}>
+    <group position={[-numColumns/2, -numColumns/2, 0]}>
       {
         columns.map((val, index) => {
           if (!val) {
@@ -88,7 +127,7 @@ const SymbolPlane = ({numColumns=25}) => {
       }
     </group>
   );
-}
+};
 
 const MatrixRain = () => {
   return (
--- src/Components/Demos/MatrixRain/SymbolShader.js
+++ src/Components/Demos/MatrixRain/SymbolShader.js
@@ -1,16 +1,20 @@
 export const SymbolShader = {
   uniforms: {
     textureAtlas: { type: "t", value: undefined },
-    initialOpacity: {value: 1.0},
+    u_time: {type: "f", value: 0.0},
+    opacity: {type: "f", value: 1.0},
   },
   vertexShader: `
     attribute vec2 textureOffsets;
+    attribute float opacity;
     varying vec2 vTextureOffsets;
+    varying float vOpacity;
     varying vec2 vUv;
     
     void main() {
       vUv = uv;   
       vTextureOffsets = textureOffsets;
+      vOpacity = opacity;
    
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
@@ -18,8 +22,9 @@ export const SymbolShader = {
   fragmentShader: `
     varying vec2 vUv;
     varying vec2 vTextureOffsets;
+    varying float vOpacity;
     uniform sampler2D textureAtlas;
-    uniform float initialOpacity;
+    uniform float u_time;
     
     // linear mapping of x from range [a1, a2] to range [b1, b2]
     float mapLinear(float x, float a1, float a2, float b1, float b2) {
@@ -34,7 +39,8 @@ export const SymbolShader = {
       texCoordinate.y = mapLinear(uv.y, 0.0, 1.0, vTextureOffsets.y, vTextureOffsets.y + 0.2);
       
       vec4 tex = vec4(texture2D(textureAtlas, texCoordinate));
-      gl_FragColor = vec4(tex.xyz, initialOpacity);
+      
+      gl_FragColor = vec4(tex.xyz, vOpacity);
 }
   `
 };
