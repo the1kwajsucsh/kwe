04dc0720a12a75e07e186af0c5871b95a8a7cf0e
The1Kwa1Jsucsh <the1kwa1jsucsh@gmail.com>
2022-08-24 22:47:55 -0400

    Revised cycling of opacities

--- src/Components/Demos/MatrixRain/MatrixRain.jsx
+++ src/Components/Demos/MatrixRain/MatrixRain.jsx
@@ -1,13 +1,75 @@
-import React, {useLayoutEffect, useRef} from "react";
+import React, {useLayoutEffect, useRef, useState} from "react";
 import {Canvas, useFrame, useLoader} from "@react-three/fiber";
 import {OrbitControls, Stats} from "@react-three/drei";
 import {BufferAttribute, DoubleSide} from "three";
 import {TextureLoader} from "three/src/loaders/TextureLoader";
 import {SymbolShader} from "./SymbolShader";
-import {randFloat, randInt} from "three/src/math/MathUtils";
+import {clamp, randInt} from "three/src/math/MathUtils";
 import {Bloom, EffectComposer} from "@react-three/postprocessing";
 
-let heightVelocities;
+function sfc32(a, b, c, d) {
+  return function() {
+    a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0;
+    let t = (a + b) | 0;
+    a = b ^ b >>> 9;
+    b = c + (c << 3) | 0;
+    c = (c << 21 | c >>> 11);
+    d = d + 1 | 0;
+    t = t + d | 0;
+    c = c + t | 0;
+    return (t >>> 0) / 4294967296;
+  }
+}
+
+const generateNewOpacityFadeAmount = (opacityFadeAmount, randFunction) => {
+  const MIN = 0.01;
+  const MAX = 0.4;
+
+  if (opacityFadeAmount === MIN) {
+    opacityFadeAmount += randFunction()/3;
+  } else if (opacityFadeAmount === MAX) {
+    opacityFadeAmount -= randFunction()/3;
+  } else {
+    let preAdjustment = opacityFadeAmount + (randFunction()-0.5)/5;
+
+    if (randFunction() < 0.5) {
+      preAdjustment = clamp(preAdjustment, 0.02, 0.1);
+    }
+
+    opacityFadeAmount = clamp(preAdjustment, MIN, MAX);
+  }
+
+  return opacityFadeAmount;
+};
+
+
+const initializeOpacityColumnIndices = (width, lengthOfCycle) => {
+  return Array.from({length: width}, () => randInt(0, lengthOfCycle - 1));
+};
+
+const initializeOpacityCycle = (length, opacityFadeAmount, rootChanceOfNewString) => {
+  let rand = sfc32(Math.random() * 80, Math.random() * 30, Math.random() * 1000, Math.random() * 20);
+
+  const array = Array.from({length: length}).fill(1);
+  let newPatternAllowed = true;
+
+  for (let i = 0; i < length; i++) {
+    if ((length-i) < 1/opacityFadeAmount) {
+      newPatternAllowed = false;
+    }
+
+    if (newPatternAllowed && rand() < rootChanceOfNewString) {
+      array[i] = 1;
+
+      opacityFadeAmount = generateNewOpacityFadeAmount(opacityFadeAmount, rand);
+    } else {
+      array[i] = Math.max(0, (array[i-1] || 0) - opacityFadeAmount);
+    }
+  }
+
+  return array;
+};
+
 
 const getNewTextureOffset = () => {
   return randInt(0, 4) * 0.2;
@@ -30,41 +92,21 @@ const updateTexture = (textureOffsetArray) => {
   }
 };
 
-const getNewOpacityVelocity = () => {
-  return randFloat(0.3, 1.3);
-};
-
 const initializeOpacities = (width, height) => {
-  heightVelocities = Array.from({length: width}, () => getNewOpacityVelocity());
-  const float32Array = new Float32Array(width*height).fill(1.0);
-
-  let id = 0;
-  for (let i = 0; i < width; i++) {
-    const initialOffset = randFloat(0, 1);
-
-    for (let j = 0; j < height; j++) {
-      float32Array[id++] = -j/height + initialOffset;
-    }
-  }
-
-  return float32Array;
+  return new Float32Array(width * height).fill(1.0);
 };
 
-const updateOpacity = (attributesArray, symbolFadeInterval, minNegativeOpacity, width, height) => {
+const updateOpacity = (attributesArray, opacityCycle, opacityColumnIndices, width, height) => {
   let index = 0;
+
   for (let i = 0; i < width; i++) {
+
+    opacityColumnIndices[i] = (opacityColumnIndices[i] + 1) % opacityCycle.length;
 
     let freeze = false;
-    if (attributesArray[index] < 0) {
-      //freeze = true;
-      heightVelocities[i] = getNewOpacityVelocity();
-    }
 
     for (let j = 0; j < height; j++) {
-      let opacity = attributesArray[index] - symbolFadeInterval*heightVelocities[i];
-      if (opacity < minNegativeOpacity) {
-        opacity = 1.0;
-      }
+      let opacity = opacityCycle[(opacityColumnIndices[i] + j) % opacityCycle.length];
 
       if (!freeze) {
         attributesArray[index] = opacity;
@@ -77,8 +119,8 @@ const updateOpacity = (attributesArray, symbolFadeInterval, minNegativeOpacity, 
 const MatrixRainPlane = ({
                            width = 50,
                            height=25,
-                           symbolFadeInterval=0.01,
-                           symbolFadePercentage=0.01,
+                           symbolFadeInterval=0.05,
+                           symbolFadePercentage=0.02,
                            minNegativeOpacity=-2,
                            position=[0, 0, 0],
                            rotation=[0, 0, 0]
@@ -87,7 +129,10 @@ const MatrixRainPlane = ({
   const positionRef = useRef();
   const opacityRef = useRef();
   const textureOffsetRef = useRef();
+  const materialRef = useRef();
   const textureAtlas = useLoader(TextureLoader, process.env.PUBLIC_URL + "/Demos/MatrixRain/atlas_matrix.png");
+  const [opacityCycle] = useState(initializeOpacityCycle(width*height*2, symbolFadePercentage, 0.05));
+  const [opacityColumnIndices] = useState(initializeOpacityColumnIndices(width, opacityCycle.length));
 
   let prevTime = 0;
   let curTime;
@@ -98,11 +143,14 @@ const MatrixRainPlane = ({
     if (curTime - prevTime > symbolFadeInterval) {
       prevTime = curTime;
 
-      updateOpacity(opacityRef.current.array, symbolFadeInterval, minNegativeOpacity, width, height);
+      updateOpacity(opacityRef.current.array, opacityCycle, opacityColumnIndices, width, height);
       opacityRef.current.needsUpdate = true;
 
       updateTexture(textureOffsetRef.current.array);
       textureOffsetRef.current.needsUpdate = true;
+
+      materialRef.current.uniforms.u_time.value = clock.elapsedTime;
+      materialRef.current.needsUpdate = true;
     }
   });
 
@@ -140,6 +188,7 @@ const MatrixRainPlane = ({
             />
           </bufferGeometry>
           <shaderMaterial
+            ref={materialRef}
             attach="material"
             args={[SymbolShader]}
             uniforms-textureAtlas-value={textureAtlas}
@@ -155,6 +204,7 @@ const MatrixRainPlane = ({
 };
 
 const MatrixRain = () => {
+
   return (
     <>
       <Canvas id="canvas" aspect={2.35} camera={{position: [0, 0, 30]}}>
--- src/Components/Demos/MatrixRain/SymbolShader.js
+++ src/Components/Demos/MatrixRain/SymbolShader.js
@@ -28,6 +28,14 @@ export const SymbolShader = {
     uniform sampler2D textureAtlas;
     uniform float u_time;
     
+    float randFromFloat(float x) {
+      return fract(sin(x)*1.0);
+    }
+    
+    float randFromVec2(vec2 co) {
+      return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+    }
+    
     // linear mapping of x from range [a1, a2] to range [b1, b2]
     float mapLinear(float x, float a1, float a2, float b1, float b2) {
       return b1 + (x - a1) * (b2 - b1) / (a2 - a1);
@@ -36,20 +44,27 @@ export const SymbolShader = {
     void main() {
       vec2 uv = vUv;
       vec2 texCoordinate;
+      float opacity = vOpacity;
       
       texCoordinate.x = mapLinear(gl_PointCoord.x, 0.0, 1.0, vTextureOffsets.x, vTextureOffsets.x + 0.2);
       texCoordinate.y = mapLinear(gl_PointCoord.y, 0.0, 1.0, vTextureOffsets.y, vTextureOffsets.y + 0.2);
       
       vec4 tex = texture2D(textureAtlas, texCoordinate);
       
-      if (vOpacity > 0.95) {
+      if (opacity > 0.99) {
         tex.r = 115.0 / 255.0;
         tex.g = 241.0 / 255.0;
         tex.b = 156.0 / 255.0;
       }
       
-      tex.a = tex.a * vOpacity;
+      // Symbol flicker
+      // if (randFromFloat(u_time) < 0.3 && randFromVec2(vTextureOffsets) < 0.001) {
+      //   opacity = 0.00;
+      // } else {
+      //   opacity = 1.0;
+      // }
       
+      tex.a = tex.a * opacity;
       gl_FragColor = tex;
 }
   `
