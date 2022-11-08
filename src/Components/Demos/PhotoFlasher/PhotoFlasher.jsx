f3f672a123ceeb8fb41e86e2cd1ff09ecd72667c
The1Kwa1Jsucsh <the1kwa1jsucsh@gmail.com>
2022-11-07 19:54:16 -0500

    Synced to beat

--- src/Components/Demos/PhotoFlasher/PhotoFlasher.jsx
+++ src/Components/Demos/PhotoFlasher/PhotoFlasher.jsx
@@ -1,8 +1,14 @@
-import React, {useRef} from "react";
+import React, {useEffect, useRef} from "react";
 import {Canvas, useFrame} from "@react-three/fiber";
 import ManualOrbitControlledPerspectiveCamera from "../../Common/ManualOrbitControlledPerspectiveCamera";
 import {Image, Stats, useTexture} from "@react-three/drei";
-import {randFloat, randInt} from "three/src/math/MathUtils";
+import {lerp, mapLinear, randFloat, randInt} from "three/src/math/MathUtils";
+import {suspend} from "suspend-react";
+import {createAudio} from "../../../js/audioAnalyzer/audioAnalyzer";
+import {Color} from "three";
+
+let lastIsBeat = false;
+let beat = false;
 
 const PhotoOverlay = ({z=0, timeOffset = 0}) => {
   const ref = useRef();
@@ -19,7 +25,7 @@ const PhotoOverlay = ({z=0, timeOffset = 0}) => {
   useFrame(({clock}) => {
     curTime = clock.getElapsedTime() + timeOffset;
 
-    if (curTime - prevTime > randFloat(2, 4)) {
+    if (beat && curTime - prevTime > randFloat(2, 4)) {
       prevTime = curTime;
 
       ref.current.visible = Math.random() > 0.6;
@@ -58,10 +64,10 @@ const PhotoBackground = ({z=0, timeOffset = 0}) => {
   useFrame(({clock}) => {
     curTime = clock.getElapsedTime() + timeOffset;
 
-    if (curTime - prevTime > randFloat(2, 4)) {
+    if (beat && curTime - prevTime > randFloat(2, 4)) {
       prevTime = curTime;
 
-      ref.current.visible = Math.random() > 0.6;
+      ref.current.visible = Math.random() > 0.2;
 
       const newTexture = maps[randInt(0, maps.length-1)];
       const newWidth = newTexture.source.data.naturalWidth;
@@ -102,7 +108,7 @@ const PhotoPlane = ({z=0, timeOffset = 0}) => {
   useFrame(({clock}) => {
     curTime = clock.getElapsedTime() + timeOffset;
 
-    if (curTime - prevTime > randFloat(1, 2)) {
+    if (beat && curTime - prevTime > 0.3) {
       prevTime = curTime;
 
       ref.current.visible = Math.random() > 0.4;
@@ -131,6 +137,43 @@ const PhotoPlane = ({z=0, timeOffset = 0}) => {
   return <Image ref={ref} position={[0, 0, z]} grayscale={1} url={process.env.PUBLIC_URL + `/img/random_photos/1.jpg`}/>
 };
 
+const PhotoEffect = ({track}) => {
+  const { gain, context, update } = suspend(() => createAudio(track, false), [track]);
+
+  useEffect(() => {
+    // Connect the gain node, which plays the audio
+    gain.connect(context.destination);
+
+    // Disconnect it on dismount
+    return () => {
+      gain.disconnect();
+    }
+  }, [gain, context]);
+
+  // let lastIsBeat = false;
+  useFrame(() => {
+    const {isBeat} = update();
+
+    // Only update the beat if it changes -- to remove triggering effect multiple times per beat
+    if (!lastIsBeat && isBeat || lastIsBeat && !isBeat) {
+      beat = isBeat;
+    }
+
+    lastIsBeat = isBeat;
+  });
+
+  return (
+    <>
+      <PhotoBackground z={-0.1}/>
+      <PhotoPlane z={0}/>
+      <PhotoPlane z={.01}/>
+      <PhotoPlane z={.02}/>
+      <PhotoPlane z={.03}/>
+      <PhotoOverlay z={.04}/>
+    </>
+  )
+};
+
 const PhotoFlasher = () => {
   return (
     <>
@@ -139,12 +182,7 @@ const PhotoFlasher = () => {
         <ambientLight/>
         <pointLight position={[10, 10, 10]}/>
         <ManualOrbitControlledPerspectiveCamera/>
-        <PhotoBackground z={-0.1} timeOffset={-0.4}/>
-        <PhotoPlane z={0}/>
-        <PhotoPlane z={.01} timeOffset={0.4}/>
-        <PhotoPlane z={.02} timeOffset={0.8}/>
-        <PhotoPlane z={.03} timeOffset={1.2}/>
-        <PhotoOverlay z={.04} timeOffset={1.6}/>
+        <PhotoEffect track={process.env.PUBLIC_URL + "/local/GKTF/05_MAAD_WORLD.mp3"}/>
         <Stats/>
       </Canvas>
     </>
