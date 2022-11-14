252879301ad2d3869fde25b4de71ff45a129fb21
The1Kwa1Jsucsh <the1kwa1jsucsh@gmail.com>
2022-11-13 20:35:59 -0500

    Square photo flasher full effect v1

--- src/Components/Demos/PhotoFlasherVisualizer/SquarePhotoFlasher.jsx
+++ src/Components/Demos/PhotoFlasherVisualizer/SquarePhotoFlasher.jsx
@@ -2,36 +2,21 @@ import React, {useLayoutEffect, useRef} from "react";
 import {Canvas, useFrame} from "@react-three/fiber";
 import {Image, Stats, useTexture} from "@react-three/drei";
 import {useControls} from "leva";
-import {lerp, randFloat, randInt, smootherstep, smoothstep} from "three/src/math/MathUtils";
+import {lerp, randFloat, randInt} from "three/src/math/MathUtils";
 import {Color} from "three"
 import ManualOrbitControlledPerspectiveCamera from "../../Common/ManualOrbitControlledPerspectiveCamera";
-import {generalSmoothStep, perlin2, seed, simplex2} from "../../../js/perlin";
+import {perlin2, seed} from "../../../js/perlin";
 
 const WHITE = new Color(`rgb(255,255,255)`);
-const IMG_URLS = Array.from({length: 20}, (_,i) => `${process.env.PUBLIC_URL}/img/colors/${i+1}.jpg`);
+const IMG_URLS = Array.from({length: 20}, (_,i) => `${process.env.PUBLIC_URL}/img/rappers/${i+1}.jpg`);
 let IMAGES;
-
-const applyGroupMove = (GROUP_MOVE, ref) => {
-  if (GROUP_MOVE) {
-    ref.current.position.x -= 0.01;
-
-    if (ref.current.position.x < -3) {
-      ref.current.position.x = 3;
-    }
-  } else {
-    ref.current.position.x = 0;
-    ref.current.position.y = 0;
-    ref.current.position.z = 0;
-  }
-};
 
 let randomPositionLastTime = 0;
 const applyRandomPosition = (RANDOM_POSITION, ref, width, height, time) => {
   const SPACING = 0.1;
-  const TIME_BETWEEN_CHANGE = 0.05;
+  const TIME_BETWEEN_CHANGE = 0.1;
 
   if (RANDOM_POSITION) {
-
     if (time - randomPositionLastTime > TIME_BETWEEN_CHANGE) {
       randomPositionLastTime = time;
 
@@ -60,7 +45,8 @@ const applyGrayscale = (GRAYSCALE, ref, width, height, time) => {
   if (GRAYSCALE) {
     for (let i = 0; i < width; i++) {
       for (let j = 0; j < height; j++) {
-        ref.current.children[i*height+j].material.uniforms.grayscale.value = true;
+        let perlin = Math.abs(perlin2(i/30 + time*2, j/50+time*0.01));
+        ref.current.children[i*height+j].material.uniforms.grayscale.value = perlin < 0.2;
       }
     }
   } else {
@@ -75,14 +61,15 @@ const applyGrayscale = (GRAYSCALE, ref, width, height, time) => {
 
 let mixInColorLastTime = 0;
 const applyMixInColor = (MIX_IN_COLOR, ref, width, height, time) => {
-  const TIME_BETWEEN_CHANGE = 0.5;
+  const TIME_BETWEEN_CHANGE = 0.03;
 
   if (MIX_IN_COLOR) {
     if (time - mixInColorLastTime > TIME_BETWEEN_CHANGE) {
       mixInColorLastTime = time;
       for (let i = 0; i < width; i++) {
         for (let j = 0; j < height; j++) {
-          ref.current.children[i * height + j].material.uniforms.color.value = new Color(`rgb(${randInt(0, 255)},${randInt(0, 255)},${randInt(0, 255)})`);
+          let perlin = Math.abs(perlin2(i/30 + time*2, j/50+time*0.01));
+          ref.current.children[i * height + j].material.uniforms.color.value = perlin < 0.1 ? new Color(`rgb(${randInt(0, 255)},${randInt(0, 255)},${randInt(0, 255)})`) : WHITE;
         }
       }
     }
@@ -101,7 +88,7 @@ const applyOpacity = (OPACITY, ref, width, height, time) => {
   if (OPACITY) {
     for (let i = 0; i < width; i++) {
       for (let j = 0; j < height; j++) {
-        const perlin = Math.abs(perlin2(i/30 + time*4, j/50+time*0.01));
+        let perlin = Math.abs(perlin2(i/30 + time*2, j/50+time*0.01));
         ref.current.children[i*height+j].material.uniforms.opacity.value = perlin;
       }
     }
@@ -117,14 +104,18 @@ const applyOpacity = (OPACITY, ref, width, height, time) => {
 
 let changeImageLastTime = 0;
 const applyChangeImage = (CHANGE_IMAGE, ref, width, height, time) => {
-  const TIME_BETWEEN_CHANGE = 0.5;
+  const TIME_BETWEEN_CHANGE = 0.03;
 
   if (CHANGE_IMAGE) {
     if (time - changeImageLastTime > TIME_BETWEEN_CHANGE) {
       changeImageLastTime = time;
       for (let i = 0; i < width; i++) {
         for (let j = 0; j < height; j++) {
-          ref.current.children[i * height + j].material.uniforms.map.value = IMAGES[randInt(0, IMAGES.length-1)]
+
+          let perlin = Math.abs(perlin2(i/30 + time*2, j/50+time*0.01));
+          if (perlin < 0.1) {
+            ref.current.children[i * height + j].material.uniforms.map.value = IMAGES[randInt(0, IMAGES.length-1)]
+          }
         }
       }
     }
@@ -132,7 +123,6 @@ const applyChangeImage = (CHANGE_IMAGE, ref, width, height, time) => {
 };
 
 // EFFECTS
-// -- Group Move (changing position of group)
 // -- Random Position & scale (otherwise default to position in a grid format)
 // -- Grayscale
 // -- Mix-in color
@@ -143,8 +133,7 @@ const EffectController = ({width=5, height=4, }) => {
 
   IMAGES = useTexture(IMG_URLS);
 
-  const {GROUP_MOVE, RANDOM_POSITION, GRAYSCALE, MIX_IN_COLOR, OPACITY, CHANGE_IMAGE} = useControls({
-    GROUP_MOVE: false,
+  const {RANDOM_POSITION, GRAYSCALE, MIX_IN_COLOR, OPACITY, CHANGE_IMAGE} = useControls({
     RANDOM_POSITION: false,
     GRAYSCALE: false,
     MIX_IN_COLOR: false,
@@ -160,7 +149,7 @@ const EffectController = ({width=5, height=4, }) => {
   useFrame(({clock}) => {
     const time = clock.getElapsedTime();
 
-    applyGroupMove(GROUP_MOVE, ref);
+    // applyGroupMove(GROUP_MOVE, ref);
     applyRandomPosition(RANDOM_POSITION, ref, width, height, time);
     applyGrayscale(GRAYSCALE, ref, width, height, time);
     applyMixInColor(MIX_IN_COLOR, ref, width, height, time);
